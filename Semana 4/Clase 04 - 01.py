0d7636119450729d86a76d25cacabe44eb049a1f
20255916 <20255916@esen.edu.sv>
2025-10-01 19:08:00 -0600

    primer commit

---
+++ Semana 4/Clase 04 - 01.py
@@ -0,0 +1,20 @@
+#creamos una variable
+usuario = "Alvin"  #variable tipo string, sirve para saber que se han guardado datos textuales
+
+#Validar el usuario
+#nada
+
+#no tengo que ir escribiendo una y otra vez el valor de una variable
+
+#Datos numéricos
+
+cantidad_alumnos = 74
+#cantidad de alumnos es un INT, es decir es un número entero
+media_edad= 18.23
+#media edad es un FLOAT, es decir un decimal
+
+print(type(cantidad_alumnos))
+print(type(media_edad))
+
+print(type(cantidad_alumnos) is int)
+print(type(media_edad) is int)
