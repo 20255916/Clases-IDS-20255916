8aff4f556f8f14bf5e4ac8901b5318d041416356
20255916 <20255916@esen.edu.sv>
2025-11-04 17:53:44 -0600

    ghj

---
+++ Estructuradatos.py
@@ -0,0 +1,24 @@
+"""
+Estructura de datos:
+           TIPOS         MUTABLE       REFERENCIA          ESTRUCTURA         PERMITE REPE?
+- String:  CHAR            sí            INDICE              "   "                SI
+- Lista:   *               sí               "                [    ]               SI
+- Tupla:     *             no               "               (    )                SI
+- Diccionario:    *        sí             CLAVES            {    }                SI
+- Set                                                                             NO
+
+"""
+
+#EJEMPLO
+#Vamos a crear un diccionario
+""""
+mi_gato= {}
+print(type(mi_gato))
+"""
+
+
+
+
+
+
+
---
+++ set.py
@@ -0,0 +1,3 @@
+colores = {"rojo", "rojo", "verde", "negro", "azul"}
+print(type(colores))
+print(len(colores))
